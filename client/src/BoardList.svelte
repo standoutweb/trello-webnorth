4ab21473b1ceb3ba68112ef702cdc224e1fb494d
lekiq <lekiqcom@gmail.com>
2024-01-15 00:01:53 +0100

    fix drawer width

--- client/src/BoardList.svelte
+++ client/src/BoardList.svelte
@@ -202,7 +202,7 @@
         position: fixed;
         right: 0;
         top: 0;
-        width: 100%;
+        width: auto;
         max-width: 700px;
         height: 100%;
         background-color: white;
