c46c6b2351a830f98e179959b424874c23fdce57
Serhii Lazarenko <lazar2007@gmail.com>
2024-05-07 16:26:15 +0300

    last changes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -10,6 +10,39 @@ const PAYMO_API_BASE_URL = "https://app.paymoapp.com/api";
 const api = express();
 const router = Router();
 
+
+// get current week number
+function getWeekNumber( d ) {
+	d = new Date( Date.UTC( d.getFullYear(), d.getMonth(), d.getDate() ) );
+	d.setUTCDate( d.getUTCDate() + 4 - ( d.getUTCDay() || 7 ) );
+	const yearStart = new Date( Date.UTC( d.getUTCFullYear(), 0, 1 ) );
+	const weekNo = Math.ceil( ( ( ( d - yearStart ) / 86400000 ) + 1 ) / 7 );
+	return weekNo;
+}
+
+function getStartAndEndDate(weekNumber) {
+	const date = new Date(new Date().getFullYear(), 0, 1);
+	date.setDate(weekNumber * 7);
+	const lastWeekEnd = new Date(date);
+	const lastWeekStart = new Date(lastWeekEnd);
+
+	lastWeekEnd.setDate(lastWeekEnd.getDate() - 1);
+	lastWeekStart.setDate(lastWeekStart.getDate() - 6);
+
+	const startDate = lastWeekStart.toISOString().split('T')[0];
+	const endDate = lastWeekEnd.toISOString().split('T')[0];
+
+	return { startDate, endDate };
+}
+
+function convertSecondsToHoursMinutes( seconds ) {
+	const hours = Math.floor( seconds / 3600 );
+	const minutes = Math.floor( ( seconds % 3600 ) / 60 );
+
+	return { hours, minutes };
+}
+
+
 router.use( ( req, res, next ) => {
 	res.header( 'Access-Control-Allow-Origin', 'http://localhost:3000' ); // Or '*' for any origin
 	res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
@@ -100,16 +133,27 @@ router.get( '/paymo/timelogs', requireAuth, async ( req, res ) => {
 	}
 } );
 
-router.get( '/paymo/timelogs/:weekNumber', requireAuth, async ( req, res ) => {
-	// todo for lazzar, let's make this flexible to get the hours worked on a specific week
-} );
+router.get( '/paymo/timelog/:weekNumber', requireAuth, async ( req, res ) => {
+	try {
+		const boardId = process.env.DAILY_BOARD_ID;
+		const weekNumber = req.params.weekNumber;
+		const response = await axios.get(`${process.env.API_URL}/boards/${boardId}/${weekNumber}/time-spent`);
+		console.log(response.data)
+		const timeInSeconds = response.data;
+		const { hours, minutes } = convertSecondsToHoursMinutes(timeInSeconds);
+		console.log(`Week ${weekNumber}`, `${hours} hours ${minutes} minutes`);
+		res.json([`Week ${weekNumber}`, `${hours} hours ${minutes} minutes`]);
+	} catch (error) {
+		console.error(`Error fetching data for week: ${weekNumber}`, error);
+		res.status(500).send(`Error fetching data for week: ${weekNumber}: ${error.toString()}`);
+	}
+});
 
 // SEND TO GOOGLE SHEETS
 router.get( '/google/:weekNumber/:timeInSeconds', requireAuth, async ( req, res ) => {
 	const timeInSeconds = req.params.timeInSeconds;
 	const weekNumber = req.params.weekNumber;
-	const hours = Math.floor( timeInSeconds / 3600 );
-	const minutes = Math.floor( ( timeInSeconds % 3600 ) / 60 );
+	const { hours, minutes } = convertSecondsToHoursMinutes( timeInSeconds );
 	try {
 		const credentials = JSON.parse( Buffer.from( process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, 'base64' ).toString( 'ascii' ) );
 		const auth = new GoogleAuth( {
@@ -157,7 +201,6 @@ router.get( '/google/:weekNumber/:timeInSeconds', requireAuth, async ( req, res 
 			}
 		};
 
-
 		const response = await sheets.spreadsheets.values.update( request );
 		res.json( response.data );
 	} catch ( error ) {
@@ -168,7 +211,16 @@ router.get( '/google/:weekNumber/:timeInSeconds', requireAuth, async ( req, res 
 
 // SEND TO SLACK CHANNEL
 router.get( '/slack', async ( req, res ) => {
-	try {
+	const today = new Date();
+	const weekNumber = getWeekNumber( today );
+	const lastWeek = weekNumber - 1;
+	const boardId = process.env.DAILY_BOARD_ID;
+	axios.get( `${ process.env.API_URL }/boards/${ boardId }/${ lastWeek }/time-spent`).then( ( response ) => {
+		const timeInSeconds = response.data;
+		const { hours, minutes } = convertSecondsToHoursMinutes( timeInSeconds );
+		res.json( [ `Week ${ lastWeek }`, `${ hours } hours ${ minutes } minutes` ] );
+	})
+	/*try {
 		const response = await axios.post( process.env.SLACK_WEBHOOK_URL, {
 			text: 'Hello from Netlify Functions!',
 		} );
@@ -176,47 +228,21 @@ router.get( '/slack', async ( req, res ) => {
 	} catch ( error ) {
 		console.error( error );
 		res.status( 500 ).send( error.toString() );
-	}
+	}*/
 } );
 
 // HELPER API ROUTES
 
-// get current week number
-router.get( '/current-week', async ( req, res ) => {
-
-	function getWeekNumber( d ) {
-		d = new Date( Date.UTC( d.getFullYear(), d.getMonth(), d.getDate() ) );
-		d.setUTCDate( d.getUTCDate() + 4 - ( d.getUTCDay() || 7 ) );
-		const yearStart = new Date( Date.UTC( d.getUTCFullYear(), 0, 1 ) );
-		const weekNo = Math.ceil( ( ( ( d - yearStart ) / 86400000 ) + 1 ) / 7 );
-		return weekNo;
-	}
-
-
-	const today = new Date();
-	const weekNumber = getWeekNumber( today );
-	res.json( { weekNumber } );
-} );
-
 // get hours worked on specific week and specific board
 router.get( '/boards/:boardId/:weekNumber/time-spent', async ( req, res ) => {
 	const { boardId, weekNumber } = req.params;
-	const date = new Date( new Date().getFullYear(), 0, 1 );
-	date.setDate( weekNumber * 7 );
-	const lastWeekEnd = new Date( date );
-	const lastWeekStart = new Date( lastWeekEnd );
-
-	lastWeekEnd.setDate( lastWeekEnd.getDate() - 1 );
-	lastWeekStart.setDate( lastWeekStart.getDate() - 6 );
-
-	const startDate = lastWeekStart.toISOString().split( 'T' )[ 0 ];
-	const endDate = lastWeekEnd.toISOString().split( 'T' )[ 0 ];
+	const { startDate, endDate } = getStartAndEndDate(weekNumber);
+	console.log(startDate, endDate);
 	const username = process.env.PAYMO_API_KEY;
 	const password = 'random'; // Use a random password as specified
 	const basicAuth = 'Basic ' + Buffer.from( username + ':' + password ).toString( 'base64' );
 
 	try {
-
 		let secretToken
 		// check if params include secret, if not continue with normal auth
 		if ( req.query.secret === process.env.SECRET_QUERY_PARAM_VALUE ) {
@@ -263,25 +289,24 @@ router.get( '/last-week-hours-daily-send-to-sheets', async ( req, res ) => {
 		return res.status( 401 ).send( 'Unauthorized' );
 	}
 
-	axios.get( `${ process.env.API_URL }/current-week` ).then( ( response ) => {
-		const lastWeek = response.data.weekNumber - 1;
-		const boardId = process.env.DAILY_BOARD_ID;
-		axios.get( `${ process.env.API_URL }/boards/${ boardId }/${ lastWeek }/time-spent`, {
+	const today = new Date();
+	const weekNumber = getWeekNumber( today );
+	const lastWeek = weekNumber - 1;
+	const boardId = process.env.DAILY_BOARD_ID;
+	axios.get( `${ process.env.API_URL }/boards/${ boardId }/${ lastWeek }/time-spent`, {
+		params: {
+			secret: process.env.SECRET_QUERY_PARAM_VALUE
+		}
+	} ).then( ( response ) => {
+		const timeInSeconds = response.data;
+		axios.get( `${ process.env.API_URL }/google/${ lastWeek }/${ timeInSeconds }/`, {
 			params: {
 				secret: process.env.SECRET_QUERY_PARAM_VALUE
 			}
 		} ).then( ( response ) => {
-			const timeInSeconds = response.data;
-			axios.get( `${ process.env.API_URL }/google/${ lastWeek }/${ timeInSeconds }/`, {
-				params: {
-					secret: process.env.SECRET_QUERY_PARAM_VALUE
-				}
-			} ).then( ( response ) => {
-				res.json( response.data );
-			} );
-		} )
+			res.json( response.data );
+		} );
 	} )
-
 } );
 
 api.use( "/api/", router );
